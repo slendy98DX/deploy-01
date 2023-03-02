deb936021832fceb42a0acedfa416ab70f926bb7
slendy98DX <marco.setaro93@gmail.com>
2023-03-02 20:27:44 +0100

    Initial commit

---
+++ deploy-01/src/main/java/co/develhope/deploy01/Deploy01Application.java
@@ -0,0 +1,13 @@
+package co.develhope.deploy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy01Application.class, args);
+	}
+
+}
